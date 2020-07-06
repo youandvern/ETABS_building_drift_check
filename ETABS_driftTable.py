a84cf9a77b09a0e13927980a5ea08adae1300ed5
Andrew Young <67914889+youandvern@users.noreply.github.com>
2020-07-06 12:44:34 -0400

    Update ETABS_driftTable.py

--- ETABS_driftTable.py
+++ ETABS_driftTable.py
@@ -3,12 +3,6 @@
 Created on Wed Mar 11 11:28:16 2020
 
 @author: Andrew-V.Young
-
-Adding comment for new pull
-
-Adding another comment for newer pull
-
-Added extra #2 comment for pull request
 """
 
 
